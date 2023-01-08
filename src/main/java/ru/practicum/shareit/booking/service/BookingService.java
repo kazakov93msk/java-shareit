5710538db2b351a4374296c44a991c47b0f7d5ca
kav-onevizion <artyom.kazakov@onevizion.com>
2023-01-08 13:50:55 +0500

    Add booking

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface BookingService {
     List<Booking> findAllByBookerId(Long bookerId, String bookingState);
+
     List<Booking> findAllByItemsOwnerId(Long ownerId, String bookingState);
+
     Booking findBookingById(Long userId, Long bookingId);
+
     Booking approveBookingById(Long userId, Long bookingId, Boolean decide);
+
     Booking createBooking(Long userId, Booking booking);
 }
